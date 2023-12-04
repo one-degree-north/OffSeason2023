f792efe9393c8867fa38ea421c0f5b0af52fcf10
Aaden Wong <aadenwong04@gmail.com>
2023-12-04 16:17:15 +0800

    indexer created

--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -2,23 +2,34 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Indexer extends SubsystemBase {
+  
 
-  private TalonFX m_Indexer = new TalonFX(1);
-
-  private final double kBeltSpeed = 1;
+  private CANSparkMax m_Indexer = new CANSparkMax(1, MotorType.kBrushless);
+  private TalonFX m_Belt1 = new TalonFX(2);
+  private TalonFX m_Belt2 = new TalonFX(3);
+  
+  private final double intakeSpeed = 1;
 
   public void index(){
-    m_Indexer.set(ControlMode.PercentOutput, kBeltSpeed);
+    m_Indexer.set(intakeSpeed);
   }
 
   public void stopIndex(){
-    m_Indexer.set(ControlMode.PercentOutput, 0);
+    m_Indexer.set(0);
   }
 
   public void outIndex(){
-    m_Indexer.set(ControlMode.PercentOutput, -kBeltSpeed);  
+    m_Indexer.set(-intakeSpeed);  
+  }
+
+  public void runBelt(){
+    m_belt
   }
 }
